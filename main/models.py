9fd3a4e233ee57606cc4551a66afa0b1b40cb5a4
ligamsi <galimov.ism@mail.ru>
2025-11-15 23:35:49 +0500

    init project and create models

---
+++ main/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils.text import slugify
+
+class League(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+class Team(models.Model):
+    league = models.ForeignKey(League, on_delete=models.CASCADE, related_name='teams')
+    name = models.CharField(max_length=100)
+    
+class Player(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='players')
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    matches = models.PositiveIntegerField(default=0)
+    goals = models.PositiveIntegerField(default=0)
+    assists = models.PositiveIntegerField(default=0)
+
+    @property
+    def ga(self):
+        return self.goals + self.assists
+    
+    def __str__(self):
+        return f"{self.name} {self.surname} ({self.team.name})"
