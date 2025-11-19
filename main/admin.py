ed8fbb8c3284d2dfb99cf177b95131291b753afa
ligamsi <galimov.ism@mail.ru>
2025-11-19 20:29:01 +0500

    Add TeamInLeague and PlayerInLeague models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import League, Team, Player, TeamInLeague, PlayerInLeague
 
-# Register your models here.
+admin.site.register(League)
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(TeamInLeague)
+admin.site.register(PlayerInLeague)
