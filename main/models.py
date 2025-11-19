f1b801c85be89189ccf3c246eac6cf3024ce5621
ligamsi <galimov.ism@mail.ru>
2025-11-19 20:25:53 +0500

    Add TeamInLeague and PlayerInLeague models

--- main/models.py
+++ main/models.py
@@ -3,7 +3,8 @@ from django.utils.text import slugify
 
 class League(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.CharField(max_length=100, unique=True)
+
+    slug = models.SlugField(unique=True, blank=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -14,20 +15,50 @@ class League(models.Model):
         return self.name
     
 class Team(models.Model):
-    league = models.ForeignKey(League, on_delete=models.CASCADE, related_name='teams')
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class TeamInLeague(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+
+    matches = models.IntegerField(default=0)
+    wins = models.IntegerField(default=0)
+    draws = models.IntegerField(default=0)
+    loses = models.IntegerField(default=0)
+    goals_scored = models.IntegerField(default=0)
+    goals_conceded = models.IntegerField(default=0)
+    difference = models.IntegerField(default=0)
+    points = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ('team', 'league')
+
+    def __str__(self):
+        return f"{self.team.name} — {self.league.name}"
     
 class Player(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='players')
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
-    matches = models.PositiveIntegerField(default=0)
-    goals = models.PositiveIntegerField(default=0)
-    assists = models.PositiveIntegerField(default=0)
-
-    @property
-    def ga(self):
-        return self.goals + self.assists
     
     def __str__(self):
-        return f"{self.name} {self.surname} ({self.team.name})"
+        return f"{self.name} {self.surname}"
+    
+class PlayerInLeague(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+
+    matches = models.IntegerField(default=0)
+    goals = models.IntegerField(default=0)
+    assists = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ('player', 'league')
+
+    def __str__(self):
+        return f"{self.player} — {self.league.name}"
+    
+    
